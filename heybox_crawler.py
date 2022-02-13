53758fd0c9eba07b877f92947b3cab366b5ec4cf
SimonHurricane5 <62279616+SimonHurricane5@users.noreply.github.com>
2022-02-13 13:40:17 +0800

    Add files via upload

---
+++ heybox_crawler.py
@@ -0,0 +1,12 @@
+import json
+
+
+
+url_heybox_search = "https://api.xiaoheihe.cn/game/search/?os_type=web&version=999.0.0&q="
+
+# 小黑盒爬虫
+def heybox_search_crawler(game_name):
+    return
+
+def heybox_lowest_crawler():
+    return
