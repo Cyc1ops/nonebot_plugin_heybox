8a5eff39de8598307a36a6a2ac3de56c82586566
SimonHurricane5 <62279616+SimonHurricane5@users.noreply.github.com>
2022-02-14 00:03:50 +0800

    Add files via upload

--- nonebot_plugin_heybox/__init__.py
+++ nonebot_plugin_heybox/__init__.py
@@ -10,21 +10,15 @@ heybox_lowest = on_command("小黑盒史低", priority=46, block=True)
 @heybox_search.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     game_name = args.extract_plain_text().strip()
-    if not args:
+    if not game_name:
         await heybox_search.finish("请输入你要查询的游戏名称捏~")
     else:
-        try:
-            mes_list = heybox_search_crawler(game_name)
-            if len(mes_list) != 1:
-                await bot.send(event, "正在搜索并生成合并消息中~", at_sender=True)
-                await bot.send_group_forward_msg(group_id=event.group_id, messages=mes_list)
-            else:
-                await bot.send(event, "无搜索结果")
-        except:
-            await bot.send(event, "出错了，请检查主机网络情况、查看运行日志")
+        result = heybox_search_crawler(game_name)
+        await heybox_search.finish(result)
 
-@heybox_lowest.handle()
-async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
 
-    msg = heybox_lowest_crawler()
-    await heybox_lowest.finish(msg)
+# @heybox_lowest.handle()
+# async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+#
+#     msg = heybox_lowest_crawler()
+#     await heybox_lowest.finish(msg)
--- nonebot_plugin_heybox/heybox_crawler.py
+++ nonebot_plugin_heybox/heybox_crawler.py
@@ -1,10 +1,11 @@
 import json
 from requests import get
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GROUP, MessageSegment, Event, Message
 
 
 url_heybox_search = "https://api.xiaoheihe.cn/game/search/?os_type=web&version=999.0.0&q="
 
-# 小黑盒爬虫
+# 小黑盒搜索游戏爬虫
 def heybox_search_crawler(game_name):
     url = url_heybox_search + game_name
     response = get(url=url).text
@@ -19,12 +20,29 @@ def heybox_search_crawler(game_name):
     initial_price=page['result']['games'][0]['price']['initial']
     current_price=page['result']['games'][0]['price']['current']
     lowest_price=page['result']['games'][0]['price']['lowest_price_raw']
-    if page['result']['games'][0]['price']['discount']!=0:
-        deadline=page['result']['games'][0]['price']['deadline']
+    discount=str(page['result']['games'][0]['price']['discount'])
+    game_pic=page['result']['games'][0]['image']
     if page['result']['games'][0]['price']['discount'] == 0:
-
+        msg = f"【游戏名】:{game}\n"+MessageSegment.image(game_pic)+f"\n现在 {game} 是原价哦\n"
+        msg = msg + f"【原价】:{initial_price}\n" +f"【当前】:{current_price}"+f"【史低】:{lowest_price}"
+    elif page['result']['games'][0]['price']['is_lowest'] == 0:
+        deadline = page['result']['games'][0]['price']['deadline']
+        msg = f"【游戏名】:{game}\n" + MessageSegment.image(game_pic) + f"\n现在 {game} 的折扣是-{discount}%,还不是史低哦\n"
+        msg = msg + f"【原价】:{initial_price}\n" +f"【当前】:{current_price}\n"+f"【史低】:{lowest_price}\n"
+        msg = msg + f"本次折扣还{deadline}"
+    elif page['result']['games'][0]['price']['is_lowest'] == 1 and page['result']['games'][0]['price']['new_lowest'] == 0:
+        deadline = page['result']['games'][0]['price']['deadline']
+        msg = f"【游戏名】:{game}\n" + MessageSegment.image(game_pic) + f"\n现在 {game} 的折扣是-{discount}%,是史低哦!!!\n"
+        msg = msg + f"【原价】:{initial_price}\n" + f"【当前】:{current_price}\n" + f"【史低】:{lowest_price}\n"
+        msg = msg + f"本次折扣还{deadline}"
+    elif page['result']['games'][0]['price']['is_lowest'] == 1 and page['result']['games'][0]['price']['new_lowest'] == 1:
+        deadline = page['result']['games'][0]['price']['deadline']
+        msg = f"【游戏名】:{game}\n" + MessageSegment.image(game_pic) + f"\n现在 {game} 的折扣是-{discount}%,是新史低!!!快买买买!!!\n"
+        msg = msg + f"【原价】:{initial_price}\n" + f"【当前】:{current_price}\n" + f"【史低】:{lowest_price}\n"
+        msg = msg + f"本次折扣还{deadline}"
+    return msg
 
     return
 
-# def heybox_lowest_crawler():
-#     return
+def heybox_lowest_crawler():
+    return
